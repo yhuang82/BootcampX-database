d2a3df3b7fd884fdbb49aed2822ddcbcf2ccca62
Yiqun Huang <albert913388672@gmail.com>
2023-10-19 19:11:11 -0700

    deal with sql injection problem using the use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,22 @@ const pool = new Pool({
   port: 5432, // Default PostgreSQL port
 });
 
-pool
-  .query(
-    `
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`
+`;
+const cohort = process.argv[2] || "JUL02";
+const values = [`${cohort}`];
+
+
+pool
+  .query(
+    queryString, values
   )
   .then((res) => {
     res.rows.forEach((row) => {
